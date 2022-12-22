89a2a68ebbf92b0d632b9d388fbaf2890480d50d
Cedar <cedarlzp@163.com>
2022-12-23 00:33:37 +0800

    四个页面搞定了

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
-export const Forth  = defineComponent({
- setup: (props, context) => {
-  return () => (
-   <div>Forth</div>
-  )
- }
-})
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloud.svg'
+import { RouterLink } from 'vue-router';
+
+export const Forth = defineComponent({
+    setup: (props, context) => {
+     return () => (
+       <div class={s.wrapper}>
+           <div class={s.card}>
+               <img src={cloud}/>
+               <h2>会挣钱<br/>还要会省钱</h2>
+           </div>
+           <div class={s.actions}>
+            <RouterLink class={s.fake} to="/start">跳转</RouterLink>
+            <RouterLink to="/start">完成</RouterLink>
+            <RouterLink class={s.fake} to="/start">跳转</RouterLink>
+           </div>
+       </div>
+     )
+    }
+   })
