e716c82d481781b3178650a5fef7888232efb14c
Cedar <cedarlzp@163.com>
2022-12-27 12:54:47 +0800

    这一次是错误提交

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,35 +1,15 @@
-import { defineComponent } from "vue";
-import s from "./WelcomeLayout.module.scss";
-import cloud from "../../assets/icons/cloud.svg";
-import { RouterLink } from "vue-router";
-import { WelcomeLayout } from "./WelcomeLayout";
+import s from "./welcome.module.scss";
+export const Forth = () => (
+  <div class={s.card}>
+    <svg>
+      <use xlinkHref="#cloud"></use>
+    </svg>
+    <h2>
+      每日提醒
+      <br />
+      不遗漏每一笔账单
+    </h2>
+  </div>
+);
 
-export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{
-          title: () => (
-            <h2>
-              每日提醒
-              <br />
-              不遗漏每一份账单
-            </h2>
-          ),
-          icon: () => <img src={cloud} />,
-          buttons: () => (
-            <>
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-              <RouterLink to="/start">完成</RouterLink>
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-            </>
-          ),
-        }}
-      </WelcomeLayout>
-    );
-  },
-});
+Forth.displayName = "Forth";
--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,33 +1,17 @@
-import { defineComponent } from "vue";
-import s from "./WelcomeLayout.module.scss";
-import chart from "../../assets/icons/chart.svg";
-import { RouterLink } from "vue-router";
-import { WelcomeLayout } from "./WelcomeLayout";
+import s from "./welcome.module.scss";
+export const Third = () => {
+  return (
+    <div class={s.card}>
+      <svg>
+        <use xlinkHref="#chart"></use>
+      </svg>
+      <h2>
+        每日提醒
+        <br />
+        不遗漏每一笔账单
+      </h2>
+    </div>
+  );
+};
 
-export const Third = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{
-          icon: () => <img src={chart} />,
-          title: () => (
-            <h2>
-              每日提醒
-              <br />
-              不遗漏每一份账单
-            </h2>
-          ),
-          buttons: () => (
-            <>
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-              <RouterLink to="/welcome/4">下一页</RouterLink>
-              <RouterLink to="/start">跳过</RouterLink>
-            </>
-          ),
-        }}
-      </WelcomeLayout>
-    );
-  },
-});
+Third.displayName = "Third";
