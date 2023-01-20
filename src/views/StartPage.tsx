e796124d4129cb8d7a17a2d1d3b8411f366e741e
Cedar <cedarlzp@163.com>
2023-01-20 18:03:48 +0800

    完成Tabs组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,4 +1,44 @@
+// import { defineComponent, ref } from "vue";
+// import { Button } from "../shared/Button";
+// import { Center } from "../shared/Center";
+// import { FloatButton } from "../shared/FloatButton";
+// import { Icon } from "../shared/Icon";
+// import { Navbar } from "../shared/Navbar";
+// import { Overlay } from "../shared/Overlay";
+// import s from "./StartPage.module.scss";
+// export const StartPage = defineComponent({
+//   setup: (props, context) => {
+//     const refOverlayVisible = ref(false);
+//     const onClickMenu = () => {
+//       refOverlayVisible.value = !refOverlayVisible.value;
+//     };
+//     return () => (
+//       <div>
+//         <Navbar>
+//           {{
+//             default: () => "记账发财",
+//             icon: () => (
+//               <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+//             ),
+//           }}
+//         </Navbar>
+//         <Center class={s.pig_wrapper}>
+//           <Icon name="pig" class={s.pig} />
+//         </Center>
+//         <div class={s.button_wrapper}>
+//           <Button class={s.button}>开始记账</Button>
+//         </div>
+//         <FloatButton iconName="add" />
+//         {refOverlayVisible.value && (
+//           <Overlay onClose={() => (refOverlayVisible.value = false)} />
+//         )}
+//       </div>
+//     );
+//   },
+// });
 import { defineComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -13,26 +53,32 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value;
     };
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => "记账发财",
-            icon: () => (
-              <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-            ),
-          }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <Button class={s.button}>开始记账</Button>
-        </div>
-        <FloatButton iconName="add" />
-        {refOverlayVisible.value && (
-          <Overlay onClose={() => (refOverlayVisible.value = false)} />
-        )}
-      </div>
+      <MainLayout>
+        {{
+          title: () => "记账发财",
+          icon: () => (
+            <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          ),
+          default: () => (
+            <>
+              <Center class={s.pig_wrapper}>
+                <Icon name="pig" class={s.pig} />
+              </Center>
+              <div class={s.button_wrapper}>
+                <RouterLink to="/items/create">
+                  <Button class={s.button}>开始记账</Button>
+                </RouterLink>
+              </div>
+              <RouterLink to="/items/create">
+                <FloatButton iconName="add" />
+              </RouterLink>
+              {refOverlayVisible.value && (
+                <Overlay onClose={() => (refOverlayVisible.value = false)} />
+              )}
+            </>
+          ),
+        }}
+      </MainLayout>
     );
   },
 });
