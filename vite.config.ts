19ac18c3fb50503c04275245292d1dfa6f447d09
Cedar <cedarlzp@163.com>
2023-01-29 14:02:15 +0800

    引入vant ui 待解决浮层bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx"
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
 
@@ -12,7 +14,10 @@ export default defineConfig({
       transformOn: true,
       mergeProps: true
     }),
-    svgstore()
+    svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   server:{
     host: '0.0.0.0'
