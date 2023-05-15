1ba46da811f8f8af6ef7a2bb95c1f3961c2e8f41
Cedar <cedarlzp@163.com>
2023-05-15 18:21:28 +0800

    收集了四个字段 kind、tag_id、happen_at 和 amount

--- src/shared/time.tsx
+++ src/shared/time.tsx
@@ -1,7 +1,24 @@
+/*
+  example
+  import { Time } from 'shared/time';
+  const time = new Time();
+  time.format('YYYY-MM-DD');
+  time.firstDayOfMonth();
+  time.firstDayOfYear();
+  time.lastDayOfMonth();
+  time.lastDayOfYear();
+  time.add(1, 'month');
+*/
 export class Time {
   date: Date;
-  constructor(date = new Date()) {
-    this.date = date;
+  constructor(date?: string | Date) {
+    if (date === undefined) {
+      this.date = new Date();
+    } else if (typeof date === "string") {
+      this.date = new Date(date);
+    } else {
+      this.date = date;
+    }
   }
   format(pattern = "YYYY-MM-DD") {
     // 目前支持的格式有 YYYY MM DD HH mm ss SSS
@@ -55,18 +72,7 @@ export class Time {
     let date = new Date(this.date.getTime());
     switch (unit) {
       case "year":
-        const currentDate = date.getDate();
-        date.setDate(1);
         date.setFullYear(date.getFullYear() + amount);
-        const targetDate = new Date(
-          date.getFullYear(),
-          date.getMonth() + 1,
-          0,
-          0,
-          0,
-          0
-        ).getDate();
-        date.setDate(Math.min(currentDate, targetDate));
         break;
       case "month":
         const d = date.getDate();
