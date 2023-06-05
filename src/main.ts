df34f2978d18705dcdc517aa867efd103fde33e7
Cedar <cedarlzp@163.com>
2023-06-06 03:26:00 +0800

    引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
+import { useMeStore } from './stores/useMeStore';
+import { routes } from './config/routes';
 import { createApp } from 'vue'
 import { App } from './App'
-import {createRouter} from "vue-router"
-import { routes } from './config/routes';
+import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
+import { createPinia } from 'pinia';
 
-import { fetchMe, mePromise } from './shared/me';
+
+const router = createRouter({ history, routes })
+const pinia = createPinia()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
+
+const meStore = useMeStore()
+meStore.fetchMe()
 
 
-const router = createRouter({history,routes})
-fetchMe()
 
 const whiteList: Record<string, 'exact' | 'startsWith'> = {
   '/': 'exact',
@@ -28,12 +37,8 @@ router.beforeEach((to, from) => {
       return true
     }
   }
-  return mePromise!.then(
+  return meStore.mePromise!.then(
     () => true,
     () => '/sign_in?return_to=' + to.path
   )
 })
-  
-  const app = createApp(App);
-  app.use(router)
-  app.mount("#app");
